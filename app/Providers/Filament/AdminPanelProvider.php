51cb289b49ce217e7756dc1ccd528a2166b0f689
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-03-02 16:34:38 +0700

    feat: add widgets

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -56,6 +56,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->plugins([
                 \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()
-            ]);
+            ])
+            ->viteTheme('resources/css/filament/admin/theme.css');
     }
 }
